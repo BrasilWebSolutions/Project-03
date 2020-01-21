e5382b5429241fb7a946db51af4cbc013d516306
Fernando Britto <me@fernandobritto.com.br>
2020-01-20 22:25:22 -0200

    First Component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Main extends Component{
+    constructor(props){
+      super(props)
+
+      this.state = {
+        novaTarefa: '',
+      }
+    }
+
     render(){
-      return <h1>Standard Flutter</h1>
+      return(
+        <div className="main">
+          <h1>Standard Flutter</h1>
+
+          <form action="#">
+            <input onChange={this.inputPlus} type="text" />
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+
+      );
     }
 }
