b216452da7274453df4affa0d290f7a1649edc21
Fernando <fernandobritto.web@gmail.com>
2022-03-02 18:48:58 -0300

    Initial commit

--- tests/testBlock.js
+++
@@ -1,9 +0,0 @@
-const Block = require('../source/block.js')
-
-const block = new Block('4931', '5SDFH559HGFH', '5SDFH559HGFH', '100')
-
-console.log(block.toString())
-console.log(Block.genesis().toString())
-const firstBlock = Block.mineBlock(Block.genesis(), '$525')
-
-console.log(firstBlock.toString())
