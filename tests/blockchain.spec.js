62247de1fb60584a8be9e43632a4653b8c960cea
Fernando <fernandobritto.web@gmail.com>
2022-03-05 08:56:08 -0300

    feature/add the method for validating a blockchain

--- tests/blockchain.spec.js
+++ tests/blockchain.spec.js
@@ -2,11 +2,12 @@ const Blockchain = require('../source/blockchain')
 const Block = require('../source/block')
 
 describe('Blockchain', () => {
-  let blockchain, data
+  let blockchain
+  let blockchain_A
 
   beforeEach(() => {
     blockchain = new Blockchain()
-    data = 'index.html'
+    blockchain_A = new Blockchain()
   })
 
   it('starts with the genesis block', () => {
@@ -14,8 +15,36 @@ describe('Blockchain', () => {
   })
 
   it('adds a new block to the chain', () => {
+    data = 'index.html'
     const block = blockchain.addBlock(data)
-
     expect(blockchain.chain[blockchain.chain.length - 1]).toEqual(block)
+  })
+
+  it('validates a valid chain', () => {
+    blockchain_A.addBlock('block A')
+    expect(blockchain.isValidChain(blockchain_A.chain)).toBe(true)
+  })
+
+  it('invalidates a chain with a corrupt genesis block', () => {
+    blockchain_A.chain[0].lastHash = 'broken'
+    expect(blockchain.isValidChain(blockchain_A.chain)).toBe(false)
+  })
+
+  it('invalidates a corrupt chain', () => {
+    blockchain_A.addBlock('block X')
+    blockchain_A.chain[1].lastHash = 'block Y'
+    expect(blockchain.isValidChain(blockchain_A.chain)).toBe(false)
+  })
+
+  it('replaces the chain with a valid chain', () => {
+    blockchain_A.addBlock('block A')
+    blockchain.replaceChain(blockchain_A.chain)
+    expect(blockchain.chain).toEqual(blockchain_A.chain)
+  })
+
+  it('does not replace the chain with one of less than or equal to the length', () => {
+    blockchain.addBlock('block A')
+    blockchain.replaceChain(blockchain_A.chain)
+    expect(blockchain.chain).not.toEqual(blockchain_A.chain)
   })
 })
